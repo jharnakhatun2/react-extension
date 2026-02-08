c876cb9ddaf3614c5c9fb98ac7fbb7371d5deedd
jharnakhatun2 <jharnakhatun203@gmail.com>
2026-02-08 10:19:18 +0600

    Created website background changer extension with colorpicker

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("")
   const onclick = async () => {
     let [tab] = await chrome.tabs.query({ active: true });
-    chrome.scripting.executeScript({
+    chrome.scripting.executeScript<string[], void>({
       target : {tabId : tab.id!},
-      func : ()=>{
-        alert("Hello my Extension!")
+      args: [color],
+      func : (color)=>{
+        document.body.style.backgroundColor = color;
       }
     })
   }
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <img src={reactLogo} className="logo react" alt="React logo" /> 
       </div>
-      <h1>Vite + React</h1>
+
+      <h1>React Extension</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <input type="color" onChange={(e)=>setColor(e.currentTarget.value)} value={color}/>
         <button onClick={onclick}>
-          Get Active Tab
+          Change Color
         </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
